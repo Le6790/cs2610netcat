bf9dc8f6830381c3189ccb8eb7e936a7c702e1d0
Erik on Voyager2 <ewfalor@gmail.com>
2017-10-02 10:48:45 -0600

    wip: updating for latest C9 instances

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install figlet python-pygments
--- run-netcat-and-html-form.sh
+++ run-netcat-and-html-form.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-N=0; while ./usleep 250000; do echo; figlet -w 130 -f small "Port 8081: Serving #$N..."; (( N++ ));  nc -l -p 8081 -q 1 < form_demo.html | pygmentize; done
+N=0;
+while sleep .25; do
+	echo
+	figlet -w 130 -f small "Port 8081: Serving #$N..."
+	(( N++ ))
+	nc -l -p 8081 -q 1 < { cat http.headers index.html } | pygmentize
+done
